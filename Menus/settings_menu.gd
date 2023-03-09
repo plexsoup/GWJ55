5e847f6a6730409c8c7e6cd3d773d5e73e1b797c
plex soup <plexsoup@gmail.com>
2023-03-09 15:06:46 -0800

    audio settings

---
+++ Menus/settings_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_master_volume_slider_drag_started():
+	pass # Replace with function body.
+
+
+func _on_master_volume_slider_drag_ended(value_changed):
+	pass # Replace with function body.

2f7b812e0c91073dafbf70bed5c04477e87dd70d
plex soup <plexsoup@gmail.com>
2023-03-10 22:42:34 -0800

    NPC and starter level

--- Scenes/Entities/NPCs/Behaviours/BackAndForth.gd
+++ Scenes/Entities/NPCs/Behaviours/BackAndForth.gd
@@ -5,6 +5,9 @@ var entity : CharacterBody3D
 var current_direction : Vector3 = Vector3.LEFT
 var speed : float = 20.0
 
+var last_poll_time : int = 0 #msec
+var polling_interval : int = 100 #msec
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,17 +17,24 @@ func init(entityObj):
 
 
 # update the virtual input controller so the NPC acts a lot like a player pressing buttons
-func _process(delta):
+func _process(_delta):
 	if entity != null and is_instance_valid(entity):
-		if current_direction.x < 0 and $RayCastLeft.is_colliding():
-			entity.input_controller.move_left = 1.0
-			entity.input_controller.move_right = 0.0
+		var ticks = Time.get_ticks_msec()
+		if ticks > last_poll_time + polling_interval:
+			last_poll_time = ticks
 			
-		elif current_direction.x > 0 and $RayCastRight.is_colliding():
-			entity.input_controller.move_right = 1.0
-			entity.input_controller.move_left = 0.0
+			check_raycasts()
+			
+func check_raycasts():
+	if current_direction.x < 0 and $RayCastLeft.is_colliding():
+		entity.input_controller.move_left = 1.0
+		entity.input_controller.move_right = 0.0
 		
-		else: # turn around, your raycast is whiffing air
-			current_direction *= -1.0
+	elif current_direction.x > 0 and $RayCastRight.is_colliding():
+		entity.input_controller.move_right = 1.0
+		entity.input_controller.move_left = 0.0
+		
+	else: # turn around, your raycast is whiffing air
+		current_direction *= -1.0
 			
 	

a3161bba636f2a722accd981bdb26380a19c4705
plexsoup <plexsoup@gmail.com>
2023-03-13 23:47:07 -0700

    added switch to turn fancy graphics off

--- global.gd
+++ global.gd
@@ -4,6 +4,12 @@ extends Node
 var current_map
 var current_player
 
+var low_spec : bool = false
+
+var user_settings = {
+	"graphics":"high",
+}
+
 var player_stats = {
 	"health_max":9,
 	"health":9,

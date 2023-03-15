ffc877be16f6d52bbafbfcbeb64d7ad1c2d98175
booleons <booleons@gmail.com>
2023-03-15 11:29:07 -0400

    switch signal, platform activation

--- Scenes/Entities/moving platform.gd
+++ Scenes/Entities/moving platform.gd
@@ -2,10 +2,11 @@ extends Node3D
 
 @export var target : Vector3
 @export var speed : int
+@export var needs_activation = false
+
+@onready 
 
 var tween
-
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +16,8 @@ func _ready():
 	tween.tween_property($StaticBody3D,"global_position",startPos + (target * scale), speed)
 	tween.tween_property($StaticBody3D, "global_position", startPos, speed)
 	tween.set_loops()
+	if needs_activation:
+		tween.pause()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,3 +27,6 @@ func _process(_delta):
 
 func _on_child_exiting_tree(_node):
 	tween.kill()
+
+func _on_switch_hit():
+	tween.play()

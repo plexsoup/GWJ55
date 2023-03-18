18bbed314e389a61c6dcb593e98af78cea00a4a6
plex soup <plexsoup@gmail.com>
2023-03-17 22:19:15 -0700

    minor bugfixes

--- Scenes/Entities/Player/ascend_stairs.gd
+++ Scenes/Entities/Player/ascend_stairs.gd
@@ -10,12 +10,15 @@ func _ready():
 
 
 func attempt_ascent(x_direction : int):
+	# must be pressing a direction and not moving forward and not be obstructed.
 	var magnitude = 1.5
 	$UpperRayCast.target_position = Vector3.RIGHT * x_direction * magnitude
 	if player.is_on_wall():
-		if $UpperRayCast.is_colliding(): # hit a platform (on layer 3)
-			return
-		else: # no obstacle, you can ascend
-			if abs(player.velocity.y) < 0.05: # approximately zero
-				Global.current_player.velocity.y += player.jump_velocity*0.67
+		var dir = Input.get_axis("move_left", "move_right")
+		if abs(dir) > 0.05:
+			if $UpperRayCast.is_colliding(): # hit a platform (on layer 3)
+				return
+			else: # no obstacle, you can ascend
+				if abs(player.velocity.y) < 0.05: # approximately zero
+					Global.current_player.velocity.y += player.jump_velocity*0.67
 		
